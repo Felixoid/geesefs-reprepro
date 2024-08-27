7b7b83bbc2b5e6d223f5492ec967afe150117019
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2024-08-27 19:25:11 +0200

    Ignore errors in the docker script

--- run.sh
+++ run.sh
@@ -22,19 +22,22 @@ mkdir -p data/r2-mount
 docker run -i -e PACKAGE_VERSIONS="$PACKAGE_VERSIONS" -e GNUPGHOME=/data/gnupg \
   -e AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID -e R2_ENDPOINT -e R2_BUCKET \
   -v ./data:/data --privileged --cap-add=SYS_ADMIN --device /dev/fuse \
-  geesefs-reproduce:latest bash -ex << 'EOF'
+  geesefs-reproduce:latest bash -x << 'EOF'
 DEB_HOST_ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
 apt install /data/reprepro_5.4.1-1_${DEB_HOST_ARCH}.deb
 "./geesefs-linux-${DEB_HOST_ARCH}" "$R2_BUCKET" /data/r2-mount -o "rw,--cheap,--file-mode=0666,--dir-mode=0777,--endpoint=$R2_ENDPOINT,--memory-limit=2050,--gc-interval=100,--max-flushers=5,--max-parallel-parts=3,--max-parallel-copy=2,dev,suid,--debug_s3,--log-file=/data/geesefs.log,-f" &
 REPRO_DIR=/data/r2-mount/reproduce
 for i in {1..20}; do
   findmnt /data/r2-mount && break || sleep 1
+  [ $i -eq 20 ] && exit 1
 done
 rm -rf "${REPRO_DIR}"
 mkdir -p "${REPRO_DIR}/configs/deb/conf"
 cp /data/distributions "${REPRO_DIR}/configs/deb/conf/distributions"
+reprepro=(reprepro --basedir "${REPRO_DIR}/configs/deb" --verbose --export=force --outdir "${REPRO_DIR}/deb")
 for version in $PACKAGE_VERSIONS; do
-  reprepro --basedir "${REPRO_DIR}/configs/deb" --verbose --export=force --outdir "${REPRO_DIR}/deb" includedeb stable /data/*"$version"*.deb
+  "${reprepro[@]}" includedeb stable /data/*"$version"*.deb
+  "${reprepro[@]}" export
   packages=()
   for package in /data/*"$version"*.deb; do
     package=$(basename "$package")
@@ -42,7 +45,8 @@ for version in $PACKAGE_VERSIONS; do
     package=${package%%_*}
     packages+=("$package=$version")
   done
-  reprepro --basedir "${REPRO_DIR}/configs/deb" --verbose --export=force --outdir "${REPRO_DIR}/deb" copy lts stable "${packages[@]}"
+  "${reprepro[@]}" copy lts stable "${packages[@]}"
+  "${reprepro[@]}" export
 done
 umount /data/r2-mount
 wait
